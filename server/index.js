334a3224ef8eaa300e02b3bace17e98c9f86542b
Eugene Kim <eugkim1997@gmail.com>
2020-08-07 16:07:04 -0700

    server handles properties get request

--- server/index.js
+++ server/index.js
@@ -1,15 +1,34 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const helpers = require('../database/helpers.js');
 
 let app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
+app.get('/properties', (req, res) => {
   // res.send('Hello World!')
-})
+  helpers.getProperties((error, response) => {
+    if (error) {
+      console.log('error:', error)
+    } else {
+      res.status(200).send(response);
+    }
+  });
+});
+
+app.post('/properties', (req, res) => {
+
+});
+
+app.get('/lists', (req, res) => {
+});
+
+app.post('/lists', (req, res) => {
+
+});
 
 const port = 8080
 app.listen(port, () => {
