ebd2fce50a86727760e5fa8e79a5425a60e73e98
Eugene Kim <eugkim1997@gmail.com>
2020-08-10 21:45:47 -0700

    configured first enzyme test

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,16 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>hi</div>
-    );
-  }
-}
+import App from './components/App.jsx'
 
 ReactDOM.render(<App />, document.getElementById('app'));
