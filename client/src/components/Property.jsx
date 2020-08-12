f00739a8a737288c8db36f7c16ddf7178d3a7eb1
Eugene Kim <eugkim1997@gmail.com>
2020-08-12 19:09:53 -0400

    minor css grid changes

--- client/src/components/Property.jsx
+++ client/src/components/Property.jsx
@@ -4,7 +4,7 @@ import styles from '../assets/stylesheets/property.css'
 var Property = (props) => {
   return (
     <div className={styles.property}>
-      <img src={props.details.image} height="120" width="150" />
+      <img src={props.details.image} height="150" width="250" />
       <div> * {props.details.rating} ({props.details.review_count})</div>
       <div>{props.details.type} . {props.details.beds} beds</div>
       <div>{props.details.name}</div>
