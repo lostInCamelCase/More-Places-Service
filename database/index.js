12ba1550e718746e147da5c4d1630c14c6eeefdd
Eugene Kim <eugkim1997@gmail.com>
2020-08-20 23:46:02 -0400

    pw change

--- database/index.js
+++ database/index.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
-  user: 'student',
-  password: 'student',
+  user: 'root',
+  password: 'password',
   database: 'morePlaces'
 });
 
